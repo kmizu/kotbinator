545ba24370c8b3eedd365c4d0da84ea1819f374e
Kota Mizushima <mizukota@gmail.com>
2016-03-01 09:20:11 +0900

    Define map method for nullable-type

--- src/main/kotlin/com/github/kmizu/kotbinator/Control.kt
+++ src/main/kotlin/com/github/kmizu/kotbinator/Control.kt
@@ -3,3 +3,15 @@ package com.github.kmizu.kotbinator
 inline fun <T> block(body: () -> T): T {
     return body()
 }
+
+inline fun <T, U> T?.map(f: (T) -> U): U? = block {
+    when(this) {
+        null -> null
+        else -> f(this)
+    }
+}
+
+fun foo() {
+    val x: String? = "FOO"
+    val y: Int? = x.map { x -> 1 }
+}
