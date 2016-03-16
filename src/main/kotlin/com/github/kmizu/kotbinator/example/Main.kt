d6253b7b460804a167850c4c0ece3d95ed0ae1b6
Kota Mizushima <mizukota@gmail.com>
2016-03-17 06:14:09 +0900

    Fix somethings

--- src/main/kotlin/com/github/kmizu/kotbinator/example/Main.kt
+++ src/main/kotlin/com/github/kmizu/kotbinator/example/Main.kt
@@ -3,21 +3,25 @@ package com.github.kmizu.kotbinator.example
 import com.github.kmizu.kotbinator.*
 
 val E: Parser<Int> by lazy {
-    (A + ((s("+") / s("-")) + A).repeat()).map { result ->
-        result.second.fold(result.first, { value, pair ->
-            if (pair.first == "+") value + pair.second else value - pair.second
-        })
+    rule {
+        (A + ((s("+") / s("-")) + A).repeat()).map { result ->
+            result.second.fold(result.first, { value, pair ->
+                if (pair.first == "+") value + pair.second else value - pair.second
+            })
+        }
     }
 }
 val A: Parser<Int> by lazy {
-    (P + ((s("*") / s("/")) + P).repeat()).map { result ->
-        result.second.fold(result.first, { value, pair ->
-            if (pair.first == "*") value * pair.second else value / pair.second
-        })
+    rule {
+        (P + ((s("*") / s("/")) + P).repeat()).map { result ->
+            result.second.fold(result.first, { value, pair ->
+                if (pair.first == "*") value * pair.second else value / pair.second
+            })
+        }
     }
 }
-val P: Parser<Int> by lazy { (s("(") seqr E seql s(")")) / numeric }
-val alphabet: Parser<String> by lazy { r('a', 'z') / r('A', 'Z') }
+val P: Parser<Int> by lazy { rule { (s("(") seqr E seql s(")")) / numeric } }
+val alphabet: Parser<String> by lazy { rule { r('a', 'z') / r('A', 'Z') } }
 val identifier: Parser<String> by lazy { alphabet.repeat1().map { a: List<String> -> a.fold("", { x, y -> x + y }) } }
 val numeric: Parser<Int> by lazy { r('0', '9').repeat1().map { v -> v.fold("", { x, y -> x + y }).toInt() } }
 val MinCSV: Parser<List<String>> by lazy { identifier rep1sep s(",") }
